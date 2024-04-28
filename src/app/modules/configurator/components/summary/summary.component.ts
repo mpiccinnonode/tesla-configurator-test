ca07eeb9601bc5d36007081b26d9c92442745923
mpiccinnonode <m.piccinno@node.coop>
2024-04-28 17:06:38 +0200

    summary

--- src/app/modules/configurator/components/summary/summary.component.ts
+++ src/app/modules/configurator/components/summary/summary.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { SelectionService } from '../../services/selection.service';
+import { Selection } from '../../models/selection.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './summary.component.html',
-  styleUrl: './summary.component.scss'
+  styleUrl: './summary.component.scss',
 })
 export class SummaryComponent {
+  currentSelection = computed<Selection>(() =>
+    this.selectionService.currentSelection(),
+  );
 
+  totalPrice = computed<number>(() => {
+    const colorPrice = this.currentSelection().color?.price ?? 0;
+    const configPrice = this.currentSelection().config?.price ?? 0;
+    const towPrice = this.currentSelection().towHitch ? this.optionalPrice : 0;
+    const yokePrice = this.currentSelection().yoke ? this.optionalPrice : 0;
+    return colorPrice + configPrice + towPrice + yokePrice;
+  });
+
+  protected readonly optionalPrice = 1000;
+
+  constructor(private selectionService: SelectionService) {}
 }
