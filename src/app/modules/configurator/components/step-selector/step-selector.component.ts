234167e85ea7a80cbc44222512ef923237c88b09
mpiccinnonode <m.piccinno@node.coop>
2024-04-28 12:33:33 +0200

    step selector routing

--- src/app/modules/configurator/components/step-selector/step-selector.component.ts
+++ src/app/modules/configurator/components/step-selector/step-selector.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-step-selector',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './step-selector.component.html',
-  styleUrl: './step-selector.component.scss'
+  styleUrl: './step-selector.component.scss',
 })
-export class StepSelectorComponent {
-
-}
+export class StepSelectorComponent {}
--- src/app/modules/configurator/configurator-routing.module.ts
+++ src/app/modules/configurator/configurator-routing.module.ts
@@ -1,11 +1,23 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { StepSelectorComponent } from './components/step-selector/step-selector.component';
+import { ModelAndColorComponent } from './components/model-and-color/model-and-color.component';
 
 const routes: Routes = [
   {
     path: '',
     component: StepSelectorComponent,
+    children: [
+      {
+        path: 'model',
+        component: ModelAndColorComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'model',
+        pathMatch: 'full',
+      },
+    ],
   },
 ];
 
